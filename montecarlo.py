b680812f49aab5f608a783fea9a1bb3c7b4fdbaf
Farhan <74499472+fb0801@users.noreply.github.com>
2021-11-13 20:30:52 +0000

    Add files via upload

--- montecarlo.py
+++ montecarlo.py
@@ -2,12 +2,12 @@
 import random
 
 
-prob=int(input('Enter probability: '))
+#prob=int(input('Enter probability: '))
 issue_name = str(input('Enter risk name: '))
 
 while issue_name !='QUIT':
 
 
     for num in range(1, 100):
-        predict=randint(1,100)
-        print("Trial",num,':',,"% chance of happening")
+        predict=random.randint(1,100)
+        print("Trial",num,' for issue',issue_name,':',predict,"% chance of happening")
