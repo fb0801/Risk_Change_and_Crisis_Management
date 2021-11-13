ce9942fc6476e6b41cbfcd3079f722ca846fccc6
Farhan <74499472+fb0801@users.noreply.github.com>
2021-11-13 20:36:25 +0000

    Add files via upload

--- montecarlo.py
+++ montecarlo.py
@@ -3,9 +3,13 @@ import random
 
 
 #prob=int(input('Enter probability: '))
-issue_name = str(input('Enter risk name: '))
+run = True
 
-while issue_name !='QUIT':
+while run:
+    issue_name = str(input('Enter risk name: '))
+    if issue_name == 'QUIT':
+        run = False
+    
 
 
     for num in range(1, 100):
