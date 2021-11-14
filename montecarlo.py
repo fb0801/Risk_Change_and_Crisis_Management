b63451a670750e5e15e545f0077063405e6d7e47
Farhan <74499472+fb0801@users.noreply.github.com>
2021-11-14 12:04:10 +0000

    Add files via upload

--- montecarlo.py
+++ montecarlo.py
@@ -13,7 +13,7 @@ while run:
     
 
 
-    for num in range(1, prob):
+    for num in range(1, prob + 1):
         #depending on user input will determine how many results are shown
         predict=random.randint(1,100)#get random value between 1-100
         print("Trial",num,' for issue',issue_name,':',str(predict) + '%',"chance of happening")
