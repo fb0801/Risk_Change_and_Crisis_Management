cf0f6baf69af1316b204113031398917c1b41ea7
Farhan Bhatti <74499472+fb0801@users.noreply.github.com>
2021-11-18 21:38:35 +0000

    Add files via upload

--- fema.py
+++ fema.py
@@ -40,7 +40,7 @@ def user_risks():
         risk_hid = int(input('Enter hideability : '))
         risk_lik = int(input('Enter likelyhood : '))
         
-        if risk_name == 'QUIT' || 'show':
+        if risk_name == 'QUIT' or 'show':
             run = False
             display_results(risk_name, risk_sev, risk_hid, risk_lik, result)
         
