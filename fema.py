19a51e0aca48bafb5b92d4a99e3cf4bc3ba8a67e
Farhan Bhatti <74499472+fb0801@users.noreply.github.com>
2021-11-18 21:15:56 +0000

    Add files via upload

--- fema.py
+++ fema.py
@@ -6,7 +6,7 @@ severity = impact
 hide = risk of us not seeing it
 '''
 
-def store_risks():
+def store_risks(risk_name, risk_sev, risk_hid, risk_lik, result):
     
 
 def display_results(risk_name, risk_sev, risk_hid, risk_lik, result):
