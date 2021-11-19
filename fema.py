a1bb14fef5b3b602fbc0795585e5d0a5ede603e7
Farhan Bhatti <74499472+fb0801@users.noreply.github.com>
2021-11-19 17:41:35 +0000

    Add files via upload

--- fema.py
+++ fema.py
@@ -46,7 +46,7 @@ def user_risks():
         
         if risk_name == 'QUIT' or 'show':
             run = False
-            display_results(risk_name, risk_sev, risk_hid, risk_lik)
+            display_results(risk_name, risk_sev, risk_hid, risk_lik, result)
         break
 
         if risk_sev and risk_hid and risk_lik in range(1,10):
