a0cdc4c9a6ded3bb0dd29b651e0c89369ea744f7
Farhan Bhatti <74499472+fb0801@users.noreply.github.com>
2021-12-04 12:40:10 +0000

    Add files via upload

---
+++ expected-value.py
@@ -0,0 +1,17 @@
+'''
+Python expected value
+'''
+
+def display():
+    
+
+def calculate():
+    #EV = p(x) * n
+
+
+
+def main():
+    print("Enter values for Expected Value")
+    prob= float(input())
+    value =int(input())
+    
